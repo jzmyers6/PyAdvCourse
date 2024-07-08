db89135174bfc40e81e525d46d723562c8f551da
JZMyers <JohnZMyers98@gmail.com>
2024-07-08 11:31:38 +0200

    version_0

---
+++ README.rst
@@ -0,0 +1,13 @@
+Bitmap-Filter
+=============
+|license|
+
+Quickstart:
+-----------
+
+<EDIT HERE> to add a codeblock to describe how to install this package using `pip`
+
+.. |license| image:: https://upload.wikimedia.org/wikipedia/en/2/22/Heckert_GNU_white.svg
+   :target: <COPYING.LESSER>
+
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='bitmapfilter',
+    version='0.1.0',
+    author="<EDIT HERE>",
+    author_email="<EDIT HERE>",
+    description='A sample project for the Python programming course',
+    packages=['bitmapfilter'],
+    cmdclass={},
+    test_suite="tests",
+    install_requires=[],
+    tests_require=[],
+    extras_require={},
+)
